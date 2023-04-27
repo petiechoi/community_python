1675719b44bc75c149d1435d68494ec6b5da2c27
alice <alice@modim.co.kr>
2023-04-27 14:26:17 +0900

    회원가입 기능 구현

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("community/", include('community.urls')),
     path("", views.index, name='index'),
+    path('common/', include('common.urls')),
 ]
